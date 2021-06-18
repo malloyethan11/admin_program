ea107b208e2df43546e23b97c922499de596f14f
Ii Blue <bluehamster648@gmail.com>
2021-06-18 17:03:02 -0400

    Changed the way frequency is handled for reports form

--- AdminProgram/My Project/Application.Designer.vb
+++ AdminProgram/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.AdminFormsDesign.frmUserLookup
+            Me.MainForm = Global.AdminFormsDesign.frmReports
         End Sub
     End Class
 End Namespace
